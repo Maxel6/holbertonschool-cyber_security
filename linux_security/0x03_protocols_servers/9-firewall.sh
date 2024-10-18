5aa65c5527afb7eeb971a43b5cea194197ec075f
Maxel6 <axelmalka@outlook.fr>
2024-10-18 12:14:38 +0200

    add -j drop

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-iptables -P INPUT DROP 
+iptables -P INPUT -j DROP 
 iptables -A INPUT -p tcp --dport ssh -j ACCEPT &&  iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
