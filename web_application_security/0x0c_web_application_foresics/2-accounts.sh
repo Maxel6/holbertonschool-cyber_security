74021b12f1c79abb6c6419f0379d4f3d2e7f7952
Maxel6 <axelmalka@outlook.fr>
2024-11-22 12:39:01 +0100

    task 2

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-tail -n 1000 auth.log | grep -i "failed" | awk '{print $11}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+
+# Extract users with failed and accepted passwords, then find common users
+common_users=$(tail -n 1000 auth.log | awk '/Failed password/ {failed[$9]} /Accepted password/ {if ($9 in failed) print $9}' | sort | uniq)
+# Print users with both failed and accepted passwords
+echo "$common_users"
