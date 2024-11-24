97348752b291f613206cf4fb570eac3884323b44
Maxel6 <axelmalka@outlook.fr>
2024-11-24 20:07:58 +0100

    task 5: list users

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk -F: '{if ($3 >= 1000 && $3 < 65534) print $1}' /etc/passwd | tr '\n' ',' | sed 's/,$/\n/'
+grep "Accepted" auth.log | awk '{print $9}' | sort | uniq | tr '\n' ',' | sed 's/,$//'
