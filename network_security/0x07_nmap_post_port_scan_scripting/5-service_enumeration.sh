0d2228624f2a0f8b08507faafc6db2ee37539974
Maxel6 <axelmalka@outlook.fr>
2024-11-26 13:52:59 +0100

    task 5: replace options by -A

--- network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap -sC -sV -O --traceroute --script "banner,ssl-enum-ciphers,default,smb-enum-domains" $1 -oN service_enumeration_results.txt
+sudo nmap -A --script "banner,ssl-enum-ciphers,default,smb-enum-domains" $1 -oN service_enumeration_results.txt
