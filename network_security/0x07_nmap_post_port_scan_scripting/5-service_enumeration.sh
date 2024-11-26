b0ab6694e58c068fa768041e70496f3aa5f4d8cd
Maxel6 <axelmalka@outlook.fr>
2024-11-26 13:55:27 +0100

    task 5: Holberton's eccentricity

--- network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/5-service_enumeration.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap -A --script "banner,ssl-enum-ciphers,default,smb-enum-domains" $1 -oN service_enumeration_results.txt
+sudo nmap -sC -sV -A -O --traceroute --script "banner,ssl-enum-ciphers,default,smb-enum-domains" $1 -oN service_enumeration_results.txt
