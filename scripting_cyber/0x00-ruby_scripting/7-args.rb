cdb16f2b70e81934b2f2ad730b58261df87126c6
Maxel6 <axelmalka@outlook.fr>
2024-12-12 17:00:24 +0100

    adding "arguments:" message

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -3,6 +3,7 @@ def print_arguments
     if ARGV.empty?
       puts "No arguments provided."
     else
+        puts "Arguments:"
       # Print each argument with its index
       ARGV.each_with_index do |arg, index|
         puts "#{index + 1}. #{arg}"
